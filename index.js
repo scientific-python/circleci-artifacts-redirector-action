88ca6d7389abc944df25e41c6cf29299e7ef741a
Eric Larson <larson.eric.d@gmail.com>
2019-10-25 16:26:19 -0400

    ENH: Actually add redirector

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
+// This as annoying because CircleCI does not use the App API.
+// Hence we must monitor statuses rather than using the more convenient
+// "checks" API.
+
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
+  try {
     core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
+    const context = github.context
+    if (['ci/circleci: build_docs', 'ci/circleci: doc', 'ci/circleci: build'].indexOf(context.payload.context) < 0) {
+      context.log('Ignoring context ' + context.payload.context)
+      return
+    }
+    if (context.payload.state === 'pending') {
+      context.log('Ignoring pending ' + context.payload.context)
+      return
+    }
+    context.log('Processing:')
+    context.log(context.payload.context)
+    context.log(context.payload.state)
+    // Adapted (MIT license) from https://github.com/Financial-Times/ebi/blob/master/lib/get-contents.js
+    const filepath = '.circleci/artifact_path'
+    var path = ''
+    try {
+      const repoData = await github.repos.getContents(context.repo({ ref: context.payload.sha, path: filepath }))
+      path = Buffer.from(repoData.data.content, 'base64').toString('utf8')
+    } catch (error) {
+      if (error.status === 404) {
+        throw new Error(`404 ERROR: file '${filepath}' not found`)
+      } else {
+        throw error
+      }
+    }
+    // Set the new status
+    const state = (context.payload.state === 'success') ? 'success' : 'neutral'
+    const buildId = context.payload.target_url.split('?')[0].split('/').slice(-1)[0]
+    const repoId = context.payload.repository.id
+    const url = 'https://' + buildId + '-' + repoId + '-gh.circle-artifacts.com/' + path
+    context.log('Linking to:')
+    context.log(url)
     core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
+    return github.repos.createStatus(context.repo({
+      sha: context.payload.sha,
+      state: state,
+      target_url: url,
+      description: 'Link to ' + path,
+      context: context.payload.context + ' artifact'
+    }))
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
