dade7e6fb95211466a792a1a111dc9af60d439da
Eric Larson <larson.eric.d@gmail.com>
2023-03-17 15:58:02 -0400

    FIX: Try again

--- index.js
+++ index.js
@@ -46,18 +46,16 @@ async function run() {
     // Get the URLs
     const artifacts_url = `https://circleci.com/api/v2/project/gh/${orgId}/${repoId}/${buildId}/artifacts`
     core.debug(`Fetching JSON: ${artifacts_url}`)
-    let headers;
     if (apiToken == null || apiToken == '') {
       apiToken = ''
     } else {
       core.debug('Successfully read CircleCI API token')
     }
-    headers = {'Circle-Token': apiToken}
+    const headers = {'Circle-Token': apiToken}
     // e.g., https://circleci.com/api/v2/project/gh/larsoner/circleci-artifacts-redirector-action/94/artifacts
     const response = await fetch(artifacts_url, { headers })
     const artifacts = await response.json()
-    const status = await response.status
-    core.debug(`Artifacts JSON (status=${status}):`)
+    core.debug(`Artifacts JSON (status=${response.status}):`)
     core.debug(artifacts)
     // e.g., {"next_page_token":null,"items":[{"path":"test_artifacts/root_artifact.md","node_index":0,"url":"https://output.circle-artifacts.com/output/job/6fdfd148-31da-4a30-8e89-a20595696ca5/artifacts/0/test_artifacts/root_artifact.md"}]}
     var url = '';
