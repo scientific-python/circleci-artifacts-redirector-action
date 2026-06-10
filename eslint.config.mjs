a97cbbbd84e16f724f01344a3c83a1dec1396c0d
Eric Larson <larson.eric.d@gmail.com>
2026-06-10 10:49:08 -0400

    Move to node24 and ES (#106)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
@@ -25,7 +20,7 @@ export default defineConfig([{
         },
 
         ecmaVersion: 2018,
-        sourceType: "commonjs",
+        sourceType: "module",
     },
 
     rules: {},
